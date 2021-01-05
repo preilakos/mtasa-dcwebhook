c7971c1e02984f67edabf8fac58b96bb66eb5657
Preil Ákos <preilakos@gmail.com>
2021-01-05 03:44:58 +0100

    New debug options, cleaner code.

--- webhookS.lua
+++ webhookS.lua
@@ -1,24 +1,24 @@
-local discordWebhookURL = "url"
+-- created by zsenel
+
+local discordWebhookURL = "url" -- eg. https://discord.com/api/webhooks/795842486513565716/RNmeiw5hyoIVy2mXbuh_J3QYaS-IxgA18tyjJgWOdcGUbnWBjbpHW_eg-AdvTJs0DcAE
 
 function sendDiscordMessage(message)
 sendOptions = {
-    queueName = "dcq",
-    connectionAttempts = 3,
-    connectTimeout = 5000,
     formFields = {
         content="```"..message.."```"
     },
 }
-fetchRemote ( discordWebhookURL, sendOptions, callback )
+fetchRemote ( discordWebhookURL, sendOptions, WebhookCallback )
 end
 
-function callback()
-outputDebugString("Message sent!")
+-- 2 arguments (responseData gives back the response or "ERROR", errorMessage gives back an integer see https://wiki.multitheftauto.com/wiki/FetchRemote for the error code list. )
+function WebhookCallback(responseData, errorMessage) 
+outputDebugString("(Discord webhook callback): responseData: "..responseData..", errorMessage: "..errorMessage)
 end
 
--- Test the function
-function test(player, command, ...)
+-- SendDiscordTestMessage (deletable, it's a sample function)
+function SendDiscordTestMessage(player, command, ...)
 local msg = table.concat({...}," ") -- for multiple words
 sendDiscordMessage(msg)
 end
-addCommandHandler("dcmessage", test)
+addCommandHandler("dcmessage", SendDiscordTestMessage)
